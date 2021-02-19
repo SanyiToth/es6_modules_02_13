329781e63ab3195de000b2cd0d682082d624634d
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-02-19 14:35:26 +0100

    es6 modules

---
+++ main.js
@@ -0,0 +1,16 @@
+import {triangle} from './triangle.js'
+import {circle} from './circle.js'
+import {square} from './square.js'
+
+
+// Use `import` and `export` to make the below code work:
+//
+// e.g. import {pizza} from "jammy"
+
+// Do not edit below this line
+
+console.log(triangle.isIsosceles(3, 7, 7) === true)
+console.log(triangle.area(4, 7) === 14)
+console.log(square.area(2, 2) === 4)
+console.log(circle.area(4.7) === 69.39778171779854)
+console.log(circle.diameter(4.7) === 9.4)
